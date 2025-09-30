8137e8f6aefa17959d889d9a1caee49b052a0a8e
Miguel Loaiza <miguel.mejial@upb.edu.co>
2025-09-30 17:00:12 -0500

    Se agregaron ejemplos de listas

---
+++ Listas/Listas1.py
@@ -0,0 +1,10 @@
+# Lista vacía
+componentes = []
+
+# Lista con elementos
+componentes = ["alas", "fuselaje", "motores", "tren de aterrizaje"]
+print(componentes[0])  # "alas"
+print(componentes[-1])  # "tren de aterrizaje"
+
+# Slicing (rebanado)
+print(componentes[1:3])  # ['alas',fuselaje', 'motores']
