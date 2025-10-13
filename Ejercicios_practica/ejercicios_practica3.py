18d82aa61b99466770671968f393b570ecc6ff0f
miguel loaiza <miguel.mejial@upb.edu.co>
2025-10-13 18:02:25 -0500

    simulador

--- Ejercicios_practica/ejercicios_practica3.py
+++ Ejercicios_practica/ejercicios_practica3.py
@@ -1,3 +1,23 @@
+def simulacion_viaje():
+  k = (float(input("ingrese la cantidad de kilometros del viaje:")))
+  g = (float(input("combustible por kilometro:")))
+  
+  if k <= 1000: 
+    combustible_requerido = g * k 
+    porcentaje = 0.10 * combustible_requerido
+    combustible_final = combustible_requerido + porcentaje  
+    
+    print(f"el combustible requerido es de {combustible_final} litros")
+ 
+  elif k > 1000:
+    combustible_requerido = g * k 
+    porcentaje_2 = 0.15 * combustible_requerido
+    combustible_n = combustible_requerido + porcentaje_2
+    print(f"el combustible requerido es de {combustible_n} litros")
+    if __name__ == "__main__":
+        simulacion_viaje()
+
+
 def suma():
     print("+ suma")
 def resta():
@@ -19,8 +39,9 @@ def menu():
         print("3. Multiplicacion")  
         print("4. Division")
         print("5. Porcentaje")
-        print("6. Salir")
-        opcion = int(input("Ingrese el numero de la operacion (1-6):"))
+        print("6. simulacion viaje")
+        print("7. Salir")
+        opcion = int(input("Ingrese el numero de la operacion (1-7):"))
         if opcion == 1:
             suma()
         elif opcion == 2:
@@ -32,6 +53,8 @@ def menu():
         elif opcion == 5:
             porcentaje()
         elif opcion == 6:
+            simulacion_viaje()
+        elif opcion == 7:
             salir()
         break
 if __name__ == "__main__":
